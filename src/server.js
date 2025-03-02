2a57477618b1c3f5ccf15b00115c4e34a8d3813a
Henrikas <hen1010@proton.me>
2025-03-02 17:28:57 +0200

    server fix 1.0

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import DevelopRouter from './routers/develop';
+import DevelopRouter from '/src/routers/develop';
 
 // env init
 dotenv.config();
