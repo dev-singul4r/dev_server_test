09fd3b97477c16115ae82b7c31a3d9378e41febe
Henrikas <hen1010@proton.me>
2025-03-02 12:29:43 +0200

    server fix

--- src/server.js
+++ src/server.js
@@ -33,13 +33,10 @@ app.listen(PORT, () => {
 // database connection
 const MONGODB_URI = process.env.MONGODB_URI;
 
-mongoose.connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => {
-    console.log('Connected to MongoDB');
-})
-.catch(err => {
-    console.error('Error connecting to Mongo: ', err.message);
-});
+mongoose.connect(MONGODB_URI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch(err => {
+        console.error('Error connecting to Mongo: ', err.message);
+    });
